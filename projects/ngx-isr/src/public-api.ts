63b08ee78eeae96fe906db2bebf40d576bfb47e1
Dima <sadchikov.d.s@gmail.com>
2022-05-29 22:09:43 +0300

    updates;

--- projects/ngx-isr/src/public-api.ts
+++ projects/ngx-isr/src/public-api.ts
@@ -9,3 +9,16 @@ export * from './lib/models';
 export { InMemoryCacheHandler } from './lib/cache-handlers';
 
 export { NgxIsrModule } from './lib/ngx-isr.module';
+
+export {
+  ISRHandlerConfig,
+  InvalidateConfig,
+  RenderConfig,
+  ServeFromCacheConfig,
+} from './lib/models/isr-handler-config';
+
+export {
+  ISROptions,
+  CacheData,
+  CacheHandler,
+} from './lib/models/cache-handler';
