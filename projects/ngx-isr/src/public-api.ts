74807dba4ef597262218bd6215bde74776d4c2f5
Dima <sadchikov.d.s@gmail.com>
2022-05-29 21:26:50 +0300

    update exports for caching models;

--- projects/ngx-isr/src/public-api.ts
+++ projects/ngx-isr/src/public-api.ts
@@ -4,14 +4,8 @@
 
 export * from './lib/ngx-isr.service';
 export * from './lib/isr-handler';
+export * from './lib/models';
 
 export { InMemoryCacheHandler } from './lib/cache-handlers';
 
 export { NgxIsrModule } from './lib/ngx-isr.module';
-
-export {
-  ISRHandlerConfig,
-  InvalidateConfig,
-  RenderConfig,
-  ServeFromCacheConfig,
-} from './lib/models/isr-handler-config';
